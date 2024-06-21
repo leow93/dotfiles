898317ab7c1748a441b9cb217061df79e06a6b0a
leow93 <leowheelan@hotmail.co.uk>
2024-06-21 14:30:47 +0100

    better fish config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set me (whoami)
+
 # Add node_modules to path
 set fish_user_paths = "./node_modules/.bin"
 
@@ -9,7 +11,7 @@ set fish_user_paths = "./node_modules/.bin"
 fish_add_path /opt/homebrew/opt/libpq/bin
 
 # Add rust tooling to path
-fish_add_path "/Users/leo/.cargo/bin"
+fish_add_path "/Users/$me/.cargo/bin"
 
 # Add golang tooling to path
 set -gx GOPATH "$HOME/go"
@@ -23,6 +25,6 @@ starship init fish | source
 [ -f .nvmrc ] && nvm use
 
 # pnpm
-set -gx PNPM_HOME /Users/leo/Library/pnpm
+set -gx PNPM_HOME /Users/$me/Library/pnpm
 set -gx PATH "$PNPM_HOME" $PATH
 # pnpm end
